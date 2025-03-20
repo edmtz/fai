cfe1c0ef81fcd0d00c537f060c9c3a4101724468
Eduardo González <eduardo_gonzm@hotmail.com>
2025-03-19 22:05:46 -0600

    Removed dist/ from repository and added to .gitignore

--- dist/routes/webhook.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const webhookController_1 = require("../controllers/webhookController");
-const router = (0, express_1.Router)();
-router.post("/webhook", webhookController_1.webhookHandler);
-exports.default = router;
